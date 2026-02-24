e3446a7ee5053ed521aaf0614d2a984bba461161
Tsuikyuu <bokusummer233@gmail.com>
2026-02-24 16:55:48 +0800

    Update package version to 1.0.0, enhance description, add repository and contributor information, and implement character insertion functionality in the plugin.

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-o-o-o-o-o-o-o-o-o-o-o-o?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-o-o-o-o-o-o-o-o-o-o-o-o)
 
-神秘念珠串
+神秘念珠串，好像可以用来插在哪里？
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, h, Schema } from 'koishi'
 
 export const name = 'o-o-o-o-o-o-o-o-o-o-o-o'
 
@@ -6,6 +6,22 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
+/** 在文本元素的每个字符间插入 - */
+function insertDashBetweenChars(elements: h[]) {
+  for (const el of elements) {
+    if (el.type === 'text' && el.attrs?.content != null) {
+      el.attrs.content = String(el.attrs.content).split('').join('-')
+    }
+    if (el.children?.length) {
+      insertDashBetweenChars(el.children)
+    }
+  }
+}
+
 export function apply(ctx: Context, config: Config) {
-  // write your plugin here
+  ctx.on('before-send', (session) => {
+    if (session.elements?.length) {
+      insertDashBetweenChars(session.elements)
+    }
+  })
 }
